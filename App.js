fc3dd6cacc2b895d7cda08a18f4acc40d251ad39
high-prog <dobriyalsarthaksa.8@gmail.com>
2023-02-15 14:41:36 +0530

    bulding homescreen

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Lets buil Uber</Text>
-      <StatusBar style="auto" />
+    <Provider store={store}>
+    <View>
+      <HomeScreen />
     </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
--- README.md
+++ README.md
@@ -7,3 +7,14 @@ A react native app with maps and other functionalities
 - React native elements
 - react navigation
 - react native maps 
+
+## Things to know
+--> In react native flexbox default is column
+--> User safeAreaView instead of view cause nowadays phones have notches and we don't want data to go there
+
+
+
+## Steps
+1. Setting up redux
+2. added tailwind-react-native-classnames fro styling
+3.
